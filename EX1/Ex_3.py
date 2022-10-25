31f10fdfdc26c01fe5e8388fdc872193bba2410e
MHKhalkhali <115968541+MHKhalkhali@users.noreply.github.com>
2022-10-25 19:30:38 +0330

    Add files via upload

---
+++ EX1/Ex_3.py
@@ -0,0 +1,20 @@
+def EX1_3(a,b):
+  '''
+(int,int)->(int)
+in this function we are calculating Maximum
+Edge of a Triangle
+
+example:
+EX1_3(8, 10) ➞ 17
+
+'''
+  if type (a) == int and type (a) == int:
+       if a>0 and b>0:
+        answer=a+b-1
+        return answer
+       else:
+         answer="Such triangle dose not exist!"
+         return answer
+  else:
+         answer="Such triangle dose not exist!"
+         return answer
