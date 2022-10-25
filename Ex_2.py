6a443323766a9d7ef134b12ff90044734704b034
MHKhalkhali <115968541+MHKhalkhali@users.noreply.github.com>
2022-10-25 18:03:21 +0330

    Add files via upload

---
+++ Ex_2.py
@@ -0,0 +1,12 @@
+def EX1_2(a,b)
+     '''
+     (int,int)->(int)
+     You should give base and height to this function to calculate
+      the Area of the triangle
+ 
+      example:
+      >>>Ex1_example(2,5)
+      >>>5
+
+     '''
+     return(a*b)/2
